3821dbfee177beff4d5b75006d93760a3957b194
yp0537 <576951401@qq.com>
2020-05-27 01:15:12 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from mitmproxy import http
 
 import requests
+import re
 
 url = "https://rgbot.peryp.com/api/bangdream/upload/"
 
@@ -11,9 +12,11 @@ uuid = b''
 class Injector:
     def response(self, flow: http.HTTPFlow) -> None:
         if flow.request.host == "api.star.craftegg.jp":
-            data = uuid + bytes([len(flow.request.url)]) + flow.request.url.encode() + flow.response.content
-            response = requests.post(url, data, headers={'Content-Type': 'application/octet-stream'}, verify=False)
-            print(response.content)
+            filter = re.findall('https://api.star.craftegg.jp/api/user/[0-9]*?/event/[0-9]*?/[a-z_]*?/[a-z]*', flow.request.url)
+            if len(filter) > 0:
+                data = uuid + bytes([len(flow.request.url)]) + flow.request.url.encode() + flow.response.content
+                response = requests.post(url, data, headers={'Content-Type': 'application/octet-stream'}, verify=False)
+                print(response.content.decode())
 
 
 addons = [Injector()]
