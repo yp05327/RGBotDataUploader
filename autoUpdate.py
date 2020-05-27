549813f3198aa44d15976bb09016aeab71795a71
yp0537 <576951401@qq.com>
2020-05-27 19:28:29 +0900

    Update autoUpdate.py

--- autoUpdate.py
+++ autoUpdate.py
@@ -1,13 +1,10 @@
 from proxyInjector import eventTopRequestHeaders, eventTopRequestUrl, postData
-from settings import proxyPort
+from settings import proxyPort, autoUpdateTimer
 
 import requests
 import os
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-
-# 1 hour don't be too fast
-delta = 3600
 
 
 def autoRequest():
@@ -27,7 +24,7 @@ def autoRequest():
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(autoRequest, 'interval', seconds=delta)
+    scheduler.add_job(autoRequest, 'interval', seconds=autoUpdateTimer)
     print('Service start. Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C    '))
 
     try:
